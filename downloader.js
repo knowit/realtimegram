9964c1be03393e94bcd254036eb1724b83b53c0b
David Ed Mellum <david@edmellum.com>
2012-09-13 14:16:24 +0200

    Added hashing of upload name in downloader

--- downloader.js
+++ downloader.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto');
 var querystring = require('querystring');
 
 var filed = require('filed');
@@ -22,20 +25,33 @@ app.post('/', function(req, res) {
 	form.onPart = function(part) {
 		// Make a new file with the same filename as the user's.
 		var filename = part.filename;
-		var newFile = filed('uploads/'+ filename);
+		var fileExt = path.extname(part.filename);
+		var filePath = __dirname + '/uploads/' + filename;
+		var newFile = filed(filePath);
 
 		// Stream file to disk as it is being sent and parsed.
 		part.pipe(newFile);
+
+		// Compute a hash of the streaming file for naming.
+		var shasum = crypto.createHash('sha1');
+		part.on('data', function(data) {
+			shasum.update(data);
+		});
 			
-		// When the file is written respond with a "201 Created".
-		// And tell the pusher a new file has been uploaded.
+		// When the file is written
 		newFile.on('end', function() {
-			res.statusCode = 201;
-			res.end();
+			// Rename image to hashed name to avoid name collisions
+			// and duplicates.
+			var newName = shasum.digest('hex') + fileExt;
+			var newPath = path.join(__dirname, '/uploads/', newName);
+			fs.rename(filePath, newPath, function(err) {
+				// Respond with a "201 Created"
+				res.statusCode = 201;
+				res.end();
 
-			console.log('File written');
-
-			updatePusher(filename);
+				// Update pusher about new upload
+				updatePusher(newName);
+			});
 		});
 	};
 
