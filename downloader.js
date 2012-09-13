d3e20a845ba8adb81c6216ccc4702f079f43328b
David Ed Mellum <david@edmellum.com>
2012-09-13 10:40:10 +0200

    Downloader

---
+++ downloader.js
@@ -0,0 +1,66 @@
+var querystring = require('querystring');
+
+var filed = require('filed');
+var express = require('express');
+var formidable = require('formidable');
+var seaport = require('seaport');
+var socketio = require('socket.io-client');
+
+var ports = seaport.connect('localhost', 5001);
+
+var app = express();
+
+app.post('/', function(req, res) {
+	res.setHeader('content-length', 0);
+
+	// Set up a parser to parse the binary multipart form data.
+	var form = new formidable.IncomingForm();
+
+	// Prepare to handle the form data.
+	form.onPart = function(part) {
+		// Make a new file with the same filename as the user's.
+		var filename = part.filename;
+		var newFile = filed('public/'+ filename);
+
+		// Stream file to disk as it is being sent and parsed.
+		part.pipe(newFile);
+			
+		// When the file is written respond with a "201 Created".
+		// And tell the pusher a new file has been uploaded.
+		newFile.on('end', function() {
+			res.statusCode = 201;
+			res.end();
+
+			console.log('File written');
+
+			updatePusher(filename);
+		});
+	};
+
+	// Start parsing the form data.
+	form.parse(req);
+});
+
+var updatePusher = function(filename) {
+		// Open a Socket.IO connection to the pusher
+		var uri = 'http://' + 'localhost' + ':' + 15094;
+
+		console.log(pusherSocket);
+
+		var pusherSocket = socketio.connect(uri);
+
+		pusherSocket.once('connect', function() {
+			console.log('connectedddd');
+			pusherSocket.emit('upload', filename);
+			pusherSocket.disconnect();
+		});
+
+		pusherSocket.on('disconnect', function () {
+			console.log('wuut');
+		});
+};
+
+// Register a service with Seaport.
+ports.service('downloader@1.0.0', function(port, ready) {
+	app.listen(port, ready);
+});
