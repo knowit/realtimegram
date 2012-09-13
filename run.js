dbf5df704ef14f705538cc527f6268cd0fdd1a66
David Ed Mellum <david@edmellum.com>
2012-09-13 18:53:04 +0200

    Use Forever to run everything

--- run.js
+++ run.js
@@ -1,7 +1,19 @@
-var spawn = require('child_process').spawn;
+var path = require('path');
 
-['router', 'pusher', 'static', 'downloader'].forEach(function(file) {
-	var ps = spawn('node', [__dirname + '/' + file + '.js']);
-	ps.stdout.pipe(process.stdout);
-	ps.stderr.pipe(process.stdout);
+var forever = require('forever-monitor');
+
+['router', 'pusher', 'static', 'downloader'].forEach(function(name) {
+	var file = name + '.js';
+	var filePath = path.join(__dirname, file);
+	var child = new (forever.Monitor)(filePath);
+
+	child.start();
+
+	child.on('restart', function() {
+		console.log(file + ' been restarted');
+	});
+
+	child.on('exit', function () {
+		console.log(file + ' has exited after 3 restarts');
+	});
 });
