5c07892ddb17c4a8cb26936b72b0f8a6e8503bf7
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2012-09-13 17:28:11 +0200

    Shorter socket.io example

--- code/socketio.js
+++ code/socketio.js
@@ -1,11 +1,4 @@
 var io = require('socket.io');
-var express = require('express');
-
-var app = express(),
-    server = require('http').createServer(app),
-    io = io.listen(server);
-
-server.listen(80);
 
 io.sockets.on('connection', function (socket) {
 	socket.emit('news', { hello: 'world' });
